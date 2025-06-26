c5938d39b1a7c1eb4f6e9eed8a8922d00f6a5032
猫宮紅葉/MomiNeko <42736006+Momiji14@users.noreply.github.com>
2025-06-26 19:17:42 +0900

    プロジェクトを作成

---
+++ src/main/kotlin/SomMiniGame/SomCore.kt
@@ -0,0 +1,14 @@
+package SomMiniGame
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SomCore : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
