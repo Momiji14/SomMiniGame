cce63b1b415f873672ac7b2ec346b6de5b3a979a
猫宮紅葉/MomiNeko <42736006+Momiji14@users.noreply.github.com>
2025-06-26 20:26:04 +0900

    Playerの構造を追加

--- src/main/kotlin/SomMiniGame/Player/Interface/PlayerExtend.kt
+++ src/main/kotlin/SomMiniGame/Player/Interface/PlayerExtend.kt
@@ -1,7 +1,10 @@
 package SomMiniGame.Player.Interface
 
+import SomMiniGame.Player.PlayerData
 import org.bukkit.entity.Player
 
 interface PlayerExtend {
+    val playerData: PlayerData
+    val uuid: String
     val player: Player
 }
--- src/main/kotlin/SomMiniGame/Player/PlayerData.kt
+++ src/main/kotlin/SomMiniGame/Player/PlayerData.kt
@@ -5,5 +5,7 @@ import SomMiniGame.Player.Interface.SaveLoad
 import org.bukkit.entity.Player
 
 class PlayerData(override val player: Player) : SaveLoad, PlayerMessage {
+    override val playerData: PlayerData = this
+    override val uuid: String = player.uniqueId.toString()
     override var isLoaded: Boolean = false
 }
